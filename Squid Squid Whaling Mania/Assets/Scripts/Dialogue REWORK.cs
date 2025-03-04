30e8f44cda0877c8b81b0b33322fcb7e0c3d9898
DylanColeman <000914931@mohawkcollege.ca>
2025-03-04 17:15:45 -0500

    Part 1 Dialogue Box

--- Squid Squid Whaling Mania/Assets/Scripts/Dialogue REWORK.cs
+++ Squid Squid Whaling Mania/Assets/Scripts/Dialogue REWORK.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DialogueREWORK : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject DialoguePanel;
+    public GameObject Button1;
+    public GameObject Button2;
+    public GameObject Button3;
+
+    public float delay = 0.1f; // Delay between letters
+
+    public TextMeshProUGUI DialogueText;
+
+    public TextMeshProUGUI Button1Text;
+    public TextMeshProUGUI Button2Text;
+    public TextMeshProUGUI Button3Text;
+
+    public string DialogueSetence;
+    public string sentence1;
+    public string sentence2;
+    public string sentence3;
+
+    private void Start()
     {
-        
+        Button1Text.text = sentence1;
+        Button2Text.text = sentence2;
+        Button3Text.text = sentence3;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        DialoguePanel.SetActive(true);
+        Button1.SetActive(false);
+        Button2.SetActive(false);
+        Button3.SetActive(false);
+        StartCoroutine(TypeSentence());
+    }
+
+
+    IEnumerator TypeSentence()
+    {
+        DialogueText.text = "";
+        foreach (char letter in DialogueSetence)
+        {
+            DialogueText.text += letter;
+            yield return new WaitForSeconds(delay);
+        }
+
+        SetButtonActive();
+    }
+
+    void SetButtonActive()
+    {
+        Button1.SetActive(true);
+        Button2.SetActive(true);
+        Button3.SetActive(true);
     }
 }
