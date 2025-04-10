b0b486a354bf7a9297fe59b5dd0390d85299aea4
DylanColeman <000914931@mohawkcollege.ca>
2025-04-10 18:56:01 -0400

    Cutscene Part 2

--- Squid Squid Whaling Mania/Assets/Scripts/EntranceBoX.cs
+++ Squid Squid Whaling Mania/Assets/Scripts/EntranceBoX.cs
@@ -5,11 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class EntranceBoX : MonoBehaviour
 {
+    public GameObject Map;
+    private bool hasPressed = false;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             SceneManager.LoadScene("Pier");
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Debug.Log("Pressed");
+            if (!hasPressed && Map != null)
+            {
+                Debug.Log("On");
+                Map.SetActive(true);
+                hasPressed = true;
+            }
+            else if (hasPressed && Map != null)
+            {
+                Debug.Log("Off");
+                Map.SetActive(false);
+                hasPressed = false;
+            }
         }
     }
 }
